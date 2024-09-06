f553f6a946507ac4beebb69db7e7d3ace9101337
jataylor111 <143449478+jataylor111@users.noreply.github.com>
2024-09-06 13:26:03 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,13 +1,3 @@
 function insertionSortReverse(arr) {
-    // Start at the very end of the array and "subtract" backwards through the index
-    for(var i = arr.length - 1; i >= 0; i--) {
-      var val = arr[i];
-      var j;
-      // Similar to the above it now just works in the opposite direction
-      for(j = i; j < arr.length - 1 && arr[j+1] < val; j++) {
-        arr[j] = arr[j+1];
-      }
-      arr[j] = val;
-    }
     return arr;
 }
